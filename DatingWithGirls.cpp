1650276e23aeafb287aaead4c3cd7150c819ff15
SB_Alwayschill <114662993+SBAlwayschill@users.noreply.github.com>
2023-12-24 19:38:52 +0545

    Add files via upload

--- DatingWithGirls.cpp
+++ DatingWithGirls.cpp
@@ -3,38 +3,46 @@ using namespace std;
 
 int main()
 {
+    //Introduction to the Program 
     cout<<"Neha is an expensive girl \n while shanu is an budget girl \n So this program will say whom and where to go on date according to your wallet! \n Good Luck Singles "<<endl;
 
+//Variable Zone started
     int money;
-    cout<<"Enter the amount you have "<<endl;
-    cin>> money;
 
-    if (money>5000)
+//Variable Zone Ended
+
+ 
+
+    cout << "Enter the amount you have " << endl; //asks for how much money you have
+
+        cin >> money; //takes input
+
+
+    //if else region started 
+
+    if (money > 5000)
     {
-        cout<<"Date with neha"<<endl;
+        cout << "Date with neha" << endl;
         if (money > 10000)
         {
-            cout<<"Go to the 5 star mall"<<endl;
+            cout << "Go to the 5 star mall" <<endl;
         }
         else
         {
-            cout<<"Go to any cafe with her brooo!!"<<endl;
+            cout << "Go to any cafe with her brooo!!" << endl;
         }
         
     }
 
     else if (money>2000)
     {
-        cout<<"Date with shanu"<<endl;
+        cout << "Date with shanu" << endl;
     }
     else
     {
-        cout<<"Go out with friends"<<endl;
+        cout << "Go out with friends" << endl;
     }
-    
-    
-
-
+    //if else region ended
 
 
 
